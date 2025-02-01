63f8b3f9f7f739f61070c0e7efc371a4f75fe2cc
AbeAndersen2207 <121773292+AbeAndersen2207@users.noreply.github.com>
2025-01-31 18:03:16 -0600

    Add files via upload
    Here are the programs for this week. Hopefully everything looks as it should. Thank you. :-)

---
+++ Program#1_Week#2.py
@@ -0,0 +1,32 @@
+#UNWSP Programming PythonCos2005DEsp25
+#Project_1_Personal Information
+#1/28/25
+#Abraham. N. Andersen
+
+#Prompt: Write a program that displays the following information.
+#Your name; Fictional address with city, state, and ZIP; Fictional telephone number; and your college major
+
+print('Name:')
+print('Abraham N Andersen')
+print('')
+print('Address:')
+print('1022 Chemville Ave S, Rileyapolis, NW  ')
+
+# This is a class i am taking this semester.
+print('')
+print('Contact Information: ')
+print('Telephone#: 462-727-0729' )
+print('')
+print('Major:')
+print('Undeclared, I am a PSEO student and still being in')
+print('11th grade I am not positive what my major will be.')
+print('I am interested in Chemistry, Music, Ancient Language')
+print('and Engineering. ')
+
+#final Salutation
+print("Thank you so much for viewing! ;)")
+print('')
+print('')
+print("To God be the Glory")
+print('Program written by: Abraham. N. Andersen')
+print('01.28.25')
---
+++ Program#4_Week#2.py
@@ -0,0 +1,29 @@
+#UNWSP Programming PythonCos2005DEsp25
+#Program_4_Temp Conversion
+#01.31.25
+#Abraham. N. Andersen
+
+# Prompt:Write a program that converts Celsius temperatures to Fahrenheit temperatures.
+# The formula is as follows: F = (9/5)C + 32
+# The program should ask the user to enter a temperature in Celsius,
+# then display the temperature converted to Fahrenheit.
+
+# I Ask the user to enter their chosen temperature in Celsius for conversion to Fahrenheit
+Celsius = float(input("You must enter a temperature in Celsius: "))
+print("")
+
+# Convert the temperature to Fahrenheit
+Fahrenheit = (9 / 5) * Celsius + 32
+
+# Display the temperature in Fahrenheit
+print("The given Celsius temperature,", Celsius, "C in Fahrenheit is:", Fahrenheit, "F")
+print("")
+print("")
+print("")
+#final Salutation
+print("Thank you so much for interacting! ;)")
+print('')
+print('')
+print("To God be the Glory")
+print('Program written by: Abraham. N. Andersen')
+print('01.31.25')
